1050255507bcc770c898c05f170d2cfe728c5368
Joseph Ortiz <josepho011235@gmail.com>
2014-11-11 18:43:43 -0500

    added fullpage.js

---
+++ app/scripts/fullpageInit.js
@@ -0,0 +1,8 @@
+	$(document).ready(function() {
+	    $('#fullpage').fullpage({
+	        //menu: '#menu',
+	        //anchors: ['firstPage', 'secondPage', '3rdPage'],
+	        //sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+	        autoScrolling: false
+	    });
+	});
