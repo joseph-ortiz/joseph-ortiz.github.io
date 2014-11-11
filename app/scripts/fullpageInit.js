70a9a1208bb33db1ef53a8ff2857ac51c0790b81
Joseph Ortiz <josepho011235@gmail.com>
2014-11-11 18:47:22 -0500

    turn off text resize on fullpage init

--- app/scripts/fullpageInit.js
+++
@@ -1,8 +0,0 @@
-	$(document).ready(function() {
-	    $('#fullpage').fullpage({
-	        //menu: '#menu',
-	        //anchors: ['firstPage', 'secondPage', '3rdPage'],
-	        //sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
-	        autoScrolling: false
-	    });
-	});
--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -17,7 +17,7 @@ app.controller('WelcomeCtrl', function($scope) {
     };
 });
 
-}).call(this,require("Wb8Gej"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_f3254e65.js","/")
+}).call(this,require("Wb8Gej"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_3b1f2ccb.js","/")
 },{"Wb8Gej":7,"angular":2,"buffer":4}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 require('./lib/angular.js');
