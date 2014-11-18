aca838dc6ebfeca78fa3a0494fccbbac779d064a
Joseph Ortiz <josepho011235@gmail.com>
2014-11-18 18:48:38 -0500

    added html5 validation and form submission

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,25 +2,35 @@
 var angular = require('angular');
 var app = angular.module('myApp', []);
 app.controller('WelcomeCtrl', function($scope) {
-    $scope.testVar = 'We are up and running from a required module!';
-}).controller("ContactCtrl", ['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
-    $scope.gotoBottom = function() {
-        // set the location.hash to the id of
-        // the element you wish to scroll to.
-        $location.hash('contact');
+        $scope.testVar = 'We are up and running from a required module!';
+    }).controller("ContactCtrl", ['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll) {
+        $scope.gotoBottom = function() {
+            // set the location.hash to the id of
+            // the element you wish to scroll to.
+            $location.hash('contact');
 
-        // call $anchorScroll()
-        $anchorScroll();
-    };
+            // call $anchorScroll()
+            $anchorScroll();
+        };
 
-}]).directive('bounceDirective', function() {
-    return {
-        restrict: 'EAC', //E = element, A = attribute, C = class, M = comment
-        replace: false,
-        template: '<h1>Joseph Ortiz</h1>',
-        link: function(scope, element) {
-                element.addClass('animated bounceInDown');
-                //element.removeClass('animated bounceInDown');
-            } //DOM manipulation
-    };
-});
+    }])
+    .controller("SubmitCtrl", ['$scope', function($scope) {
+        $scope.success = false;
+        $scope.animate = "";
+        $scope.submit = function() {
+            $scope.success = true;
+            $scope.animate = "animated pulse";
+        };
+
+
+    }]).directive('bounceDirective', function() {
+        return {
+            restrict: 'EAC', //E = element, A = attribute, C = class, M = comment
+            replace: false,
+            template: '<h1>Joseph Ortiz</h1>',
+            link: function(scope, element) {
+                    element.addClass('animated bounceInDown');
+                    //element.removeClass('animated bounceInDown');
+                } //DOM manipulation
+        };
+    });
